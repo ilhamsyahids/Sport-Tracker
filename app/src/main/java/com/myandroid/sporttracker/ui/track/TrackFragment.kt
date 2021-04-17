775a7b8e055065cca6187f70b31e15ae8c3c7326
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-17 19:21:30 +0700

    request location permission when in track fragment

--- app/src/main/java/com/myandroid/sporttracker/ui/track/TrackFragment.kt
+++ app/src/main/java/com/myandroid/sporttracker/ui/track/TrackFragment.kt
@@ -1,37 +1,89 @@
 package com.myandroid.sporttracker.ui.track
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.myandroid.sporttracker.R
+import com.myandroid.sporttracker.util.TrackingUtil
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class TrackFragment : Fragment() {
+class TrackFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var trackViewModel: TrackViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         trackViewModel =
-                ViewModelProvider(this).get(TrackViewModel::class.java)
+            ViewModelProvider(this).get(TrackViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_track, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         trackViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
+        requestPermissions()
+
         root.findViewById<View>(R.id.fab).setOnClickListener {
             findNavController().navigate(R.id.action_nav_track_to_trackingFragment)
         }
         return root
+    }
+
+
+    private fun requestPermissions() {
+        if (TrackingUtil.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                0,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                0,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
