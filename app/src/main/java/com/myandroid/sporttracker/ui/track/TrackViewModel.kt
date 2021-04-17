355191aeb577c606002b995781195043a5f60134
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-17 15:05:14 +0700

    change dashboard fragment to track fragment

--- app/src/main/java/com/myandroid/sporttracker/ui/track/TrackViewModel.kt
+++ app/src/main/java/com/myandroid/sporttracker/ui/track/TrackViewModel.kt
@@ -1,13 +1,13 @@
-package com.myandroid.sporttracker.ui.dashboard
+package com.myandroid.sporttracker.ui.track
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class TrackViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is track Fragment"
     }
     val text: LiveData<String> = _text
 }
