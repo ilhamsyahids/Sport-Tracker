67c9608cc8d6b8f17c19bd01d4090754c643c108
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-18 11:07:57 +0700

    start service with notification

--- app/src/main/java/com/myandroid/sporttracker/MainActivity.kt
+++ app/src/main/java/com/myandroid/sporttracker/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.myandroid.sporttracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.myandroid.sporttracker.util.Constant.TRACKING_ACTION_SHOW_TRACKING_FRAGMENT
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToIntent(intent)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -29,5 +34,16 @@ class MainActivity : AppCompatActivity() {
                     else -> navView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToIntent(intent)
+    }
+
+    private fun navigateToIntent(intent: Intent?) {
+        if (intent?.action == TRACKING_ACTION_SHOW_TRACKING_FRAGMENT) {
+            nav_host_fragment.findNavController().navigate(R.id.action_main_trackingFragment)
+        }
     }
 }
--- app/src/main/java/com/myandroid/sporttracker/services/TrackingService.kt
+++ app/src/main/java/com/myandroid/sporttracker/services/TrackingService.kt
@@ -1,18 +1,39 @@
 package com.myandroid.sporttracker.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.myandroid.sporttracker.MainActivity
+import com.myandroid.sporttracker.R
+import com.myandroid.sporttracker.util.Constant.NOTIFICATION_TRACKING_SERVICE_CHANNEL_ID
+import com.myandroid.sporttracker.util.Constant.NOTIFICATION_TRACKING_SERVICE_CHANNEL_NAME
+import com.myandroid.sporttracker.util.Constant.NOTIFICATION_TRACKING_SERVICE_ID
 import com.myandroid.sporttracker.util.Constant.TRACKING_ACTION_PAUSE_SERVICE
+import com.myandroid.sporttracker.util.Constant.TRACKING_ACTION_SHOW_TRACKING_FRAGMENT
 import com.myandroid.sporttracker.util.Constant.TRACKING_ACTION_START_OR_RESUME_SERVICE
 import com.myandroid.sporttracker.util.Constant.TRACKING_ACTION_STOP_SERVICE
 
 class TrackingService: LifecycleService() {
 
+    var isFirstRun = true
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 TRACKING_ACTION_START_OR_RESUME_SERVICE -> {
+                    if (isFirstRun) {
+                        startServiceForeground()
+                        isFirstRun = false
+                    }
                     Log.d("TrackingService", "Started or resume service")
                 }
                 TRACKING_ACTION_PAUSE_SERVICE -> {
@@ -28,4 +49,42 @@ class TrackingService: LifecycleService() {
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun startServiceForeground() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_TRACKING_SERVICE_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_run)
+            .setContentTitle("Sport Tracker")
+            .setContentText("00:00:00")
+            .setContentIntent(setIntentToMainActivity())
+
+        startForeground(NOTIFICATION_TRACKING_SERVICE_ID, notificationBuilder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_TRACKING_SERVICE_CHANNEL_ID,
+            NOTIFICATION_TRACKING_SERVICE_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun setIntentToMainActivity() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = TRACKING_ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
 }
