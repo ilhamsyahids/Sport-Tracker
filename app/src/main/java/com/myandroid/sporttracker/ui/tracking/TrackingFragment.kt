2f07fcb75ec495e8baa0489fa89b24e75536eab1
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-18 10:17:56 +0700

    add google maps

--- app/src/main/java/com/myandroid/sporttracker/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/com/myandroid/sporttracker/ui/tracking/TrackingFragment.kt
@@ -6,15 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.myandroid.sporttracker.R
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
-class TrackingFragment : Fragment() {
+class TrackingFragment : Fragment(), OnMapReadyCallback {
 
     companion object {
         fun newInstance() = TrackingFragment()
     }
 
     private lateinit var viewModel: TrackingViewModel
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,4 +35,56 @@ class TrackingFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in ITB and move the camera
+        val itb = LatLng(-6.891192, 107.610447)
+        mMap.addMarker(MarkerOptions().position(itb).title("Marker in ITB"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(itb, 16f))
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mapView?.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        mapView?.onSaveInstanceState(outState)
+    }
 }
