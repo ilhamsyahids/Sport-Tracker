16465d0e83f37ecc459372ffaf0e7bf581491aed
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-18 09:48:14 +0700

    remove appbar

--- app/src/main/java/com/myandroid/sporttracker/MainActivity.kt
+++ app/src/main/java/com/myandroid/sporttracker/MainActivity.kt
@@ -19,9 +19,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_track, R.id.nav_scheduler))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         navController
