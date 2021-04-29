dc61a5ad9c306b91c24daf1c80ed066bf018338c
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-04-30 01:00:30 +0700

    add schedule add fragment

---
+++ app/src/main/java/com/myandroid/sporttracker/ui/schedule/ScheduleAddFragment.kt
@@ -0,0 +1,19 @@
+package com.myandroid.sporttracker.ui.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.myandroid.sporttracker.R
+
+class ScheduleAddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_schedule_add, container, false)
+    }
+}
--- app/src/main/java/com/myandroid/sporttracker/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/myandroid/sporttracker/ui/schedule/ScheduleFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.myandroid.sporttracker.R
 
 class ScheduleFragment : Fragment() {
@@ -21,6 +20,12 @@ class ScheduleFragment : Fragment() {
     ): View? {
         scheduleViewModel =
                 ViewModelProvider(this).get(ScheduleViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        val root = inflater.inflate(R.layout.fragment_schedule, container, false)
+
+        root.findViewById<View>(R.id.fabSchedule).setOnClickListener {
+            findNavController().navigate(R.id.action_nav_schedule_to_scheduleAddFragment)
+        }
+
+        return root
     }
 }
